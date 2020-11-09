7ee87f9ab32f8f1563696f3f8bc95bb05e064b03
Stas_Krasnagir <stas220693@gmail.com>
2020-11-09 17:18:45 +0300

    09-11-2020

--- two_sum.py
+++ two_sum.py
@@ -1,10 +1,10 @@
-def twoSum(nums, target):
+def two_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
 
-
 nums = [7, 11, 7, 5, 21, 9, 4]
 target = 9
 print(twoSum(nums, target))
+print("Hello world")
